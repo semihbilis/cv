2892e320be567064b9cf670fd91c49305547ec93
Muhammed Semih BİLİŞ <91787565+semihbilis@users.noreply.github.com>
2021-11-11 18:32:49 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -8,16 +8,22 @@ AOS.init({
 
 var lang=0;
 
-function language()
+function languageChange()
 {
-  if(lang==0)
+  fetch("scripts/CV.json",
   {
-    lang=1;
-  }
-  else
-  {
-    lang=0;
-  }
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json()).then(data =>
+    {
+      lang += 1;
+      if (lang >= Object.keys(data.lang).length)
+      {
+        lang = 0;
+      }
+    })
   dataLoading();
 }
 
@@ -29,55 +35,49 @@ function dataChanged(element, text)
 
 function dataLoading()
 {
-  fetch("scripts/CV.json", {
+  fetch("scripts/CV.json",
+  {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     }
-  }).then(response => response.json()).then(data => {
+  }).then(response => response.json()).then(data =>
+    {
     dataChanged("SiteTitle", data.siteTitle[lang] + " | "+ data.updatedText[lang] +": " + data.cvUpdated);
+
     dataChanged("About",data.about[lang]);
     dataChanged("Skill",data.skill[lang]);
-    dataChanged("CSkill",data.skill[lang]);
     dataChanged("Experience",data.experience[lang]);
-    dataChanged("CExperience",data.experience[lang]);
     dataChanged("Education",data.education[lang]);
     dataChanged("CEducation",data.education[lang]);
     dataChanged("Contact",data.contact[lang]);
     dataChanged("CContact",data.contact[lang]);
     dataChanged("Language",data.language[lang]+"("+data.lang[lang]+")");
+
     dataChanged("NameSurname", data.nameSurname);
     dataChanged("Title", data.title[lang]);
+
     dataChanged("DButtonTitle", data.dButtonTitle[lang]);
     dataChanged("BContactTitle", data.bContactTitle[lang]);
+
     dataChanged("TAboutTitle", data.tAboutTitle[lang]);
-    var tAText = tAboutText();
-    function tAboutText()
+    if (document.getElementById("TAbout").innerHTML != null)
     {
-      var t = "";
-      if (lang == 0)
-      {
-        for (var key in data.tAbout.tr) {
-          t += data.tAbout.tr[key] + "<br><br>";
-        }
-        return t;
-      }
-      else if (lang == 1)
-      {
-        for (const key in data.tAbout.en) {
-          t += data.tAbout.en[key] + "<br><br>";
-        }
-        return t;
-      }
+      document.getElementById("TAbout").innerHTML = "";  
     }
-    dataChanged("tAbout", tAText);
+    for (const key in data.tAbout[lang])
+    {
+      document.getElementById("TAbout").innerHTML += data.tAbout[lang][key] +"<br><br>";
+    }
+
     dataChanged("PersonalInfoTitle", data.personalInfoTitle[lang]);
     dataChanged("PhoneNumberTitle", data.phoneNumberTitle[lang]);
     dataChanged("MailTitle", data.mailTitle[lang]);
     dataChanged("AddressTitle", data.addressTitle[lang]);
     dataChanged("AgeTitle", data.ageTitle[lang]);
     var tAge = ageCalucate();
-    function ageCalucate() {
+    function ageCalucate()
+    {
       bYear=data.birthDay.year;
       bMonth=data.birthDay.month;
       bDay=data.birthDay.day;
@@ -105,6 +105,8 @@ function dataLoading()
     dataChanged("DrivingLicense", data.drivingLicense[lang]);
     dataChanged("MilitaryServiceTitle", data.militaryServiceTitle[lang]);
     dataChanged("MilitaryService", data.militaryService[lang]);
+
+    dataChanged("CSkill",data.skill[lang]);
     dataChanged("SkillPrograms", data.skillPrograms[lang]);
     dataChanged("SkillSDT", data.skillSDT[lang]);
     dataChanged("SkillFLTitle", data.skillFLTitle[lang]);
@@ -115,6 +117,44 @@ function dataLoading()
     dataChanged("SkillFLRBeginner", data.skillFLBeginner[lang]);
     dataChanged("SkillFLWBeginner", data.skillFLBeginner[lang]);
 
+    dataChanged("CExperience",data.experience[lang]);
+    if (document.getElementById("ExperienceWork").innerHTML != null)
+    {
+      document.getElementById("ExperienceWork").innerHTML = "";
+    }
+    for (const key in data.experienceWork) 
+    {
+      document.getElementById("ExperienceWork").innerHTML += "<div class='timeline-card timeline-card-info' data-aos='fade-in' data-aos-delay='0'>"+
+      "<div class='timeline-head px-4 pt-3'>"+
+        "<div class='h5'>"+
+          "<label id='WorkTitle'>"+
+          data.experienceWork[key].workTitle[lang]+
+          "</label>"+
+          "<span class='text-muted h6'>"+
+            "<label id='CompanyText'>&nbsp;"+
+            data.companyText[lang]+
+            "</label>"+
+            "<label id='CompanyName'>&nbsp;"+
+            data.experienceWork[key].companyName[lang]+
+            "</label>"+
+          "</span>"+
+        "</div>"+
+      "</div>"+
+      "<div class='timeline-body px-4 pb-4'>"+
+        "<div class='text-muted text-small mb-3'>"+
+        data.experienceWork[key].dateWork[lang]+
+        "</div>"+
+        "<div>"+
+        data.experienceWork[key].description[lang]+
+        "</div>"+
+      "</div>";
+    }
+
+    dataChanged("SoftwareDevelopmentTitle", data.softwareDevelopmentTitle[lang]);
+
+
+    
+    
 
 
 
